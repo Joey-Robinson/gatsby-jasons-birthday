743a64701542534aba44a9231fd2da92d2a35598
Joey Robinson <theamazingness@gmail.com>
2019-06-26 14:18:45 -0400

    Gatsby Project for Jason's birthday. YEET

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Header = () => (
+  <header>
+    <h1>Hi Jason&#x2757;</h1>
+  </header>
+)
+
+export default Header
---
+++ src/components/layout.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Header from "./header"
+import "./layout.css"
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+    </>
+  )
+}
+
+export default Layout
